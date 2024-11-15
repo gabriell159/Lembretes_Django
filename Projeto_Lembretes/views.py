22332901e8b71d2de4561bf72210843bc5b3f764
gabriell159 <gfo2130626@gmail.com>
2024-11-15 11:31:52 -0300

    Redirecionamento de link e criação do forms

--- Projeto_Lembretes/views.py
+++ Projeto_Lembretes/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Topic
+from .forms import TopicForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 def index(request):
@@ -16,3 +19,17 @@ def topic(request,topic_id):
     entries = topic.entry_set.order_by('-data_added')
     context = {'topic':topic , 'entries':entries }
     return render(request, 'Projeto_Lembretes/topic.html', context)
+
+def new_topic(request):
+    if request.method != 'POST':
+        # Nenhum dado enviado,criacao do formulario
+        form = TopicForm()
+    else:
+        # Dados de post submetido,processa os dados
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #reversed usa o name de URLS para redirecionar,nao é a melhor pratica
+            return HttpResponseRedirect(reversed('topic'))
+    context = {'form': form}
+    return render(request, 'Projeto_Lembretes/new_topic.html', context)
