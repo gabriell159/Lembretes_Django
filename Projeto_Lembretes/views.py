49744c78f2dc26f70413a53f66359be5ad3c2031
gabriell159 <gfo2130626@gmail.com>
2024-12-02 10:45:35 -0300

    utilização do forms para salvar dados

--- Projeto_Lembretes/views.py
+++ Projeto_Lembretes/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
@@ -30,6 +30,20 @@ def new_topic(request):
         if form.is_valid():
             form.save()
             #reversed usa o name de URLS para redirecionar,nao é a melhor pratica
-            return HttpResponseRedirect(reversed('topic'))
+            return HttpResponseRedirect(reverse('topics'))
     context = {'form': form}
     return render(request, 'Projeto_Lembretes/new_topic.html', context)
+
+def new_entry(request,topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('topic', args=[topic_id]))
+    context = {'topic':topic,'form':form}
+    return render(request, 'Projeto_Lembretes/new_entry.html', context)
