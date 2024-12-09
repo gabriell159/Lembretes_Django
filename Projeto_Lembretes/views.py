1318f90951caa31b80d4e07b98f8790ad9213868
gabriell159 <gfo2130626@gmail.com>
2024-12-09 10:21:15 -0300

    Edição dos tópicos

--- Projeto_Lembretes/views.py
+++ Projeto_Lembretes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Topic
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
@@ -47,3 +47,18 @@ def new_entry(request,topic_id):
             return HttpResponseRedirect(reverse('topic', args=[topic_id]))
     context = {'topic':topic,'form':form}
     return render(request, 'Projeto_Lembretes/new_entry.html', context)
+
+def edit_entry(request,entry_id):
+    entry = Entry.object.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('topic',args=[topic.id]))
+
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'Projeto_Lembretes/edit_entry.html', context)
